255792f07e5182f9697118b0933aece2ea4c00b9
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-02-21 21:56:52 +0300

    Add experiences styles

--- src/components/ExperienceItem.js
+++ src/components/ExperienceItem.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const ExperienceItem = props => (
   <div className="experience-item">
-    <div className="title">{props.itemTitle}</div>
-    <div className="time">{props.itemTime}</div>
+    <header>
+      <div className="title">{props.itemTitle}</div>
+      <div className="time">{props.itemTime}</div>
+    </header>
     <div className="desc">{props.itemDesc}</div>
   </div>
 )
