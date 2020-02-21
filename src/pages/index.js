63dcbe07c7b5963d87eb3a42383b557f32f8c803
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-02-21 15:46:33 +0300

    Add hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const Index = () => (
+  <div id="hero">
+    <div>
+      <h1>
+        Hi, I’m <b>Peyman</b>, a front-end {`<developer />`} focused on building
+        products that are beautiful from the inside out.
+      </h1>
+      <div className="actions">
+        <a href="#" className="btn">
+          Say hi
+        </a>
+        <a href="#" className="btn">
+          Resume
+        </a>
+      </div>
+    </div>
+  </div>
+)
+
+export default Index
