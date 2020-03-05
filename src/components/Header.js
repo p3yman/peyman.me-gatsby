600cb7979712b6da33c1d7ae908115504dcbf4af
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-03-05 20:42:47 +0300

    Add basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 import React from "react"
+import Logo from "./../assets/images/logo.svg"
 
 export default () => (
-  <div className="container">
-    <span className="menu-lines">
-      {[1, 2, 3, 4, 5].forEach(i => (
-        <span className="Hi" key={i.toString()}>
-          <span></span>
-        </span>
-      ))}
-    </span>
-  </div>
+  <header id="header">
+    <div className="container">
+      <div id="header-content">
+        <div className="logo">
+          <img src={Logo} alt="Peyman's Logo" width="40" />
+        </div>
+        <ul id="menu">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Playlists</a>
+          </li>
+          <li>
+            <a href="#">Newsletter</a>
+          </li>
+          <li>
+            <a href="#">Uses</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#" className="btn">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
 )
