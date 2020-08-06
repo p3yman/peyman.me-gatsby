351a7f40cd01b6387c21f4b21149b88f4b34e0e5
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-08-06 10:39:17 +0300

    Rearrenge footer grammer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ export default () => (
   <div id="footer">
     <div className="copyright">
       {new Date().getFullYear()}. Inspired from a lot of websites and developed
-      by copying from tutorials by Peyman (just kidding{" "}
+      by copying from tutorials (just kidding{" "}
       <span role="img" aria-label="Grimacing Face">
         😬
       </span>
-      )
+      ) by <strong>Peyman</strong>.
       <br />
     </div>
   </div>
