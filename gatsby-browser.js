abc38dd6c83c44f87270b10d1c293273680f1c04
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-08-08 18:45:30 +0300

    Add prismjs to highlight codes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,3 @@
-import "./src/assets/scss/main.scss"
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/plugins/command-line/prism-command-line.css";
+import "./src/assets/scss/main.scss";
