e9c22f843af8a55136fe2795e1840fee422a6e76
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-03-17 15:53:58 +0100

    Add vercel analytics separatly

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Analytics } from '@vercel/analytics/react';
+
 import Seo from "../components/SEO"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
@@ -9,6 +11,7 @@ const Layout = props => (
     <Header />
     {props.children}
     <Footer />
+    <Analytics />
   </div>
 )
 
